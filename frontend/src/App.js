9a2f6e6bb14188beb935a433e4b0bedaff7cb255
Youssef97dev <youssefmoussafi97@gmail.com>
2023-02-14 08:54:24 -1100

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 // Libs
 import { Routes, Route } from "react-router-dom";
 
+// Routes
+import LoggedInRoutes from "./routes/LoggedInRoutes";
+import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
+
 // Pages
 import Login from "./pages/login";
 import Profile from "./pages/profile";
@@ -9,9 +13,13 @@ import Home from "./pages/home";
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} exact />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/" element={<Home />} />
+      <Route element={<LoggedInRoutes />}>
+        <Route path="/profile" element={<Profile />} exact />
+        <Route path="/" element={<Home />} />
+      </Route>
+      <Route element={<NotLoggedInRoutes />}>
+        <Route path="/login" element={<Login />} exact />
+      </Route>
     </Routes>
   );
 }
